77ff73cdaf95965e00c14ae7b027af33986770f2
Mikhail Zhigulin <perseus.apps@gmail.com>
2022-02-09 10:43:25 +0700

    Make preparations for the project
    This commit provides the Xcode project prepared for development.
    
    ADDED: Xcode Project for iOS 9.0.
    
    ADDED: Unit Testing Bundle.

---
+++ darkmodediscovery/darkmodediscovery/main.swift
@@ -0,0 +1,40 @@
+//
+//  main.swift
+//  darkmodediscovery
+//
+//  Created by Mikhail Zhigulin on 09.02.2022.
+//
+
+import UIKit
+
+// MARK: - The Application Object Initiation
+
+let appDelegateClass: AnyClass = NSClassFromString("TestingAppDelegate") ?? AppDelegate.self
+
+UIApplicationMain(CommandLine.argc,
+                  CommandLine.unsafeArgv,
+                  nil,
+                  NSStringFromClass(appDelegateClass))
+
+// MARK: - The Application Delegate
+
+class AppDelegate: UIResponder { var window: UIWindow? }
+
+extension AppDelegate: UIApplicationDelegate
+{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        #if DEBUG
+        print(">> Launching with real app delegate")
+        print(">> [\(type(of: self))]." + #function)
+        #endif
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        window!.rootViewController = MainViewController.storyboardInstance()
+        window!.makeKeyAndVisible()
+        
+        return true
+    }
+}
---
+++ darkmodediscovery/darkmodediscoveryTests/TestingAppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  TestingAppDelegate.swift
+//  darkmodediscoveryTests
+//
+//  Created by Mikhail Zhigulin on 09.02.2022.
+//
+
+import XCTest
+
+// MARK: - The Testing Application Delegate
+
+@objc(TestingAppDelegate)
+class TestingAppDelegate: UIResponder, UIApplicationDelegate
+{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+                     launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        print("<< Launching with testing app delegate")
+        print("<< \(type(of: self)) " + #function)
+        
+        return true
+    }
+}
