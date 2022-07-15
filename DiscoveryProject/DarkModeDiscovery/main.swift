ed2ce7336d0d7bfe50599ff0f6340ac18bfa5142
Mikhail Zhigulin <mzhigulin@gmail.com>
2022-07-15 19:06:17 +0700

    ADDED: license extra mention
    This commit provides license extra mention to source code.
    
    ADDED: License extra mention to source code.

--- DiscoveryProject/DarkModeDiscovery/main.swift
+++ DiscoveryProject/DarkModeDiscovery/main.swift
@@ -5,6 +5,7 @@
 //  Created by Mikhail Zhigulin in 7530.
 //
 //  Copyright © 7530 Mikhail Zhigulin of Novosibirsk.
+//  Licensed under the special license. See LICENSE file.
 //  All rights reserved.
 //
 
