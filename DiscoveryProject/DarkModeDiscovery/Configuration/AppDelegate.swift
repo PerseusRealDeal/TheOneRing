912c2606916ad8555a7dddb542d0b1ad3b09e773
Mikhail <mzhigulin@gmail.com>
2023-02-04 12:52:50 +0700

    INCLUDED: AppDelegate explicit
    This commit goes with explicit app delegate.

---
+++ DiscoveryProject/DarkModeDiscovery/Configuration/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  DarkModeDiscovery
+//
+//  Created by Mikhail Zhigulin in 7530.
+//
+//  Copyright © 7530 - 7531 Mikhail Zhigulin of Novosibirsk.
+//
+//  Licensed under the MIT license. See LICENSE file.
+//  All rights reserved.
+//
+
+import UIKit
+
+/// The app delegate.
+class AppDelegate: UIResponder { var window: UIWindow? }
+
+/// Delegate behaviors.
+extension AppDelegate: UIApplicationDelegate {
+    /// Gives entry instructions on starting.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        #if DEBUG
+        print(">> Launching with business matter purpose")
+        print(">> [\(type(of: self))]." + #function)
+        #endif
+
+        // Register Settings Bundle
+        registerSettingsBundle()
+
+        // Init the app's window
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        // Give it a root view for the first screen
+        window!.rootViewController = MainViewController.storyboardInstance()
+        window!.makeKeyAndVisible()
+
+        // And, finally, apply a new style for all screens
+        // AppearanceService.makeUp()
+
+        return true
+    }
+}
